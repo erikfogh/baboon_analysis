e9ab555255103d6b3122a1a944164b084be4a27e
erik <qulli.erik@gmail.com>
2021-02-15 17:57:20 +0100

    updated internal structure

--- baboon_first_analysis/workflow.py
+++
@@ -1,9 +0,0 @@
-from gwf import Workflow, AnonymousTarget
-import os
-import glob
-import pandas as pd
-import genominterv
-from groups import Group
-
-gwf = Workflow(defaults={"account": "baboondiversity"})
-
